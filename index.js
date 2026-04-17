3ac5b4b27021f18ed714dadedc4dd1d28bcfd043
Mr-Hassaan1 <md.hassaan.tahir@gmail.com>
2026-04-17 13:17:11 +0500

    update

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const urlRoutes = require('./routers/url.js');
 const { connectToMongoDB } = require('./connect.js');
-const URL = require('./models/url.js')
+const URL = require('./models/url.js');
 
 const app = express();
 const PORT = 8001;
 
 connectToMongoDB('mongodb://localhost:27017/short-url')
     .then(() => console.log('MongoDB Connected Successfully!'))
-    .catch((err) => console.log('Connection Error', err))
+    .catch((err) => console.log('Connection Error', err));
 
 app.use(express.json());
 
@@ -19,13 +19,13 @@ app.get('/:shortId', async (req, resp) => {
     const entry = await URL.findOneAndUpdate({
         shortId
     }, {
-        $push : {
-            visitHistory : {
-                timestamp : Date.now()
+        $push: {
+            visitHistory: {
+                timestamp: Date.now()
             }
         }
     })
-    resp.redirect(entry.redirectURL)
+    resp.redirect(entry.redirectURL);
 });
 
 app.listen(PORT, () => console.log('Server Started at PORT :', PORT));
